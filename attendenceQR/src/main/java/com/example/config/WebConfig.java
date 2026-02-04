c51c77203ca54ff0a6ff1783d61cd38803cee617
weebonweed <yashop370@gmail.com>
2026-02-05 00:04:17 +0530

    Update WebConfig.java

--- attendenceQR/src/main/java/com/example/config/WebConfig.java
+++ attendenceQR/src/main/java/com/example/config/WebConfig.java
@@ -1,5 +1,27 @@
 package com.example.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
 public class WebConfig {
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
 }
